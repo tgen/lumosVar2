122f7bfa09496ed9a33087b6c342e666c7164143
Rebecca Halperin <rhalperin@tgen.org>
2016-10-17 10:08:50 -0700

    make min start difference input parameter

--- src/makeFstart.m
+++ src/makeFstart.m
@@ -1,10 +1,11 @@
-function fStart=makeFstart(fOld)
+function fStart=makeFstart(fOld,minStartDiff)
 
 m=1;
 distMat=ones(size(fOld,1),1000);
 fStart=[];
-while m./max(distMat(:))>0.2
-    fRand=100*rand(size(fOld,1),1000);
+while m./max(distMat(:))>minStartDiff
+    %fRand=100*rand(size(fOld,1),1000);
+    fRand=100*betarnd(inputParam.alphaF,(inputParam.alphaF-1)./inputParam.priorF-inputParam.alphaF+2,size(fOld,1),1000);
     distMat=squareform(pdist([fOld fStart fRand]','cityblock'));
     minList=min(distMat(:,1:size(fOld,2)+size(fStart,2)),[],2);
     [m,idx]=max(minList(size(fOld,2)+size(fStart,2)+1:end));
