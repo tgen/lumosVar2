4375070d77c154aebcc6650e2f7bee696f1ad939
Rebecca Halperin <rhalperin@tgen.org>
2017-02-08 11:37:45 -0700

    add per sample sample fraction prior

--- src/makeFstart.m
+++ src/makeFstart.m
@@ -1,11 +1,11 @@
-function fStart=makeFstart(fOld,inputParam)
+function fStart=makeFstart(fOld,tIdx,inputParam)
 
 m=1;
 distMat=ones(size(fOld,1),1000);
 fStart=[];
 while m./max(distMat(:))>inputParam.minStartDiff
     %fRand=100*rand(size(fOld,1),1000);
-    fRand=100*betarnd(inputParam.alphaF,(inputParam.alphaF-1)./inputParam.priorF-inputParam.alphaF+2,size(fOld,1),1000);
+    fRand=100*betarnd(ones(length(tIdx),1000)*inputParam.alphaF,((inputParam.alphaF-1)./inputParam.priorF(tIdx)-inputParam.alphaF+2)*ones(1,1000),size(fOld,1),1000);
     distMat=squareform(pdist([fOld fStart fRand]','cityblock'));
     minList=min(distMat(:,1:size(fOld,2)+size(fStart,2)),[],2);
     [m,idx]=max(minList(size(fOld,2)+size(fStart,2)+1:end));
---
+++ src/nllCNAaddClone_v2.m
@@ -0,0 +1,94 @@
+function nll = nllCNAaddClone(hetPos,somPos,Tcell,exonRD,segsMerged,inputParam,CNAscale,W,fOld,fNew)
+%nllCNA - computes negative loglikliehood of copy number parameters
+%
+% Syntax: nll = nllCNA(dataHet,dataSom,exonRD,segs,inputParam,param)
+%
+% Inputs:
+%   dataHet: data for germline heterozygous positions with columns:
+%       1-'Chr',2-'Pos',3-'ControlRD',4-'TumorRD',5-'Bcount'
+%   dataSom: data for somatic positions with columns:
+%       1-'Chr',2-'Pos',3-'ControlRD',4-'TumorRD',5-'Bcount'
+%   exonRD: matrix of exon data with columns: 1-'Chr',2-'StartPos',3-'EndPos',
+%       4-'TumorRD',5-'NormalRD',6-'MapQC',7-'perReadPass',8-'abFrac'
+%   segs: matrix of segment data with columns:
+%       1-'Chr',2-'StartPos',3-'EndPos',4-'segmentMean Tumor/Normal Log Ratio'
+%   inputParam: structure with fields: cnaPrior, minAllelePrior, minLik,
+%       alphaF, priorF
+%   param: vector of paramaters of length 2*numClones+1
+%       param(1) - copy number scaling constant
+%       param(2:numClones+1)=W (controls width of allele frequency dist)
+%       param(numClones+1:2*numClones+1)=f (sample fractions)
+%   
+% Outputs:
+%   nll: negative log likelihood
+%
+% Other m-files required: none
+% Subfunctions: none
+% MAT-files required: none
+%
+% See also: TumorOnlyWrapper, fitCNA, callCNA
+
+% Author: Rebecca F. Halperin, PhD
+% Translational Genomics Research Institute
+% email: rhalperin@tgen.org
+% Website: https://github.com/tgen
+% Last revision: 3-June-2016
+
+%------------- BEGIN CODE --------------
+
+%%% read in inputs
+E=table();
+E.Chr=exonRD{1}(:,1);
+E.StartPos=exonRD{1}(:,2);
+E.EndPos=exonRD{1}(:,3);
+for i=1:length(Tcell)
+    E.TumorRD(:,i)=exonRD{i}(:,4);
+    E.NormalRD(:,i)=exonRD{i}(:,5);
+end
+
+f=[fOld fNew(:)]./100;
+
+[N, M, Ftable, ~, cnaIdx, nllCNA]=callCNAmulti_v2(hetPos,Tcell,exonRD,segsMerged,inputParam,[CNAscale(:); W(:); f(:)]);
+segsTable=array2table(segsMerged(:,1:3),'VariableNames',{'Chr','StartPos','EndPos'});
+segsTable.N=N;
+segsTable.M=M;
+segsTable.F=Ftable;
+segsTable.cnaIdx=cnaIdx;
+
+for j=1:length(Tcell)
+    T=Tcell{j}(somPos,:);
+    idx=getPosInRegionSplit([T.Chr T.Pos],segsTable{:,1:3},inputParam.blockSize);
+    T.NumCopies=segsTable.N(idx);
+    T.MinAlCopies=segsTable.M(idx);
+    T.cnaF=segsTable.F(idx,j);
+    Tsom{j}=T;
+end
+
+[postComb, ~,pDataComb,clones,~]=jointSNV_v2(Tsom, f, W, inputParam);
+for j=1:length(Tsom)
+    [lia,locb]=ismember([Tsom{j}.Chr Tsom{j}.Pos],[postComb.Chr postComb.Pos],'rows');
+    somLik(:,j)=pDataComb{j}.Somatic(locb);
+    somIdx(:,j)=clones(locb);
+end
+
+tIdx=setdiff(1:length(Tcell),inputParam.NormalSample);
+%%% find likelihood of somatic variant
+if(sum(somPos)>0)
+    %fMax=max(f,[],1);
+    for j=1:length(tIdx)
+        priorF(:,j)=betapdf(f(j,somIdx(:,tIdx(j))),inputParam.alphaF,(inputParam.alphaF-1)./inputParam.priorF(tIdx(j))-inputParam.alphaF+2)+inputParam.minLik;
+    end
+else
+    somLik=1;
+    priorF=1;
+end
+    
+%end
+%priorF=1;
+
+%%% sum negative log likliehoods
+%nll=sum((-sum(log(somLik))-sum(log(hetlikMax))-sum(log(depthlikMax))-sum(log(priorCNAMax))-sum(log(priorMinAlleleMax))-sum(log(priorF))-nansum(log(priorCNAf)))./(length(somLik)+length(hetlikMax)+length(depthlikMax)+length(priorCNAMax)+length(priorMinAlleleMax)+length(priorF)+sum(~isnan(priorCNAf))));
+%nll=sum((-sum(log(somLik))-sum(log(hetlikMax))-sum(log(depthlikMax))))./(length(somLik)+length(hetlikMax)+length(depthlikMax));
+
+nll=sum(-sum(log(somLik)./(inputParam.priorSomaticSNV*sum(E.EndPos-E.StartPos)))-mean(log(priorF)))+nllCNA;
+return;
