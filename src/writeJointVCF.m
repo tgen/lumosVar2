8cd740a714e08e4ed62a71d005ef0ba01311b4b4
Rebecca Halperin <rhalperin@tgen.org>
2018-01-26 16:50:57 -0700

    changed num2str conversion for speed

--- src/writeJointVCF.m
+++ src/writeJointVCF.m
@@ -177,38 +177,84 @@ AltNT(strncmp('SomaticPair',Filter,10) & T.RefComb~=T.Acomb,:)=int2ntIndels(T.Ac
 %Info=cellstr(strcat('DP=',num2str(T.ReadDepth,'%-.0f'),';DPQC=',num2str(T.ReadDepthPass,'%-.0f')));
 %Info(T.RefComb==T.Acomb,:)=strcat(Info(T.RefComb==T.Acomb,:),';AF=',num2str(T.BcountsComb(T.RefComb==T.Acomb,:)./T.ReadDepthPass(T.RefComb==T.Acomb,:),'%-.3f'));
 %Info(T.RefComb==T.Bcomb,:)=strcat(Info(T.RefComb==T.Bcomb,:),';AF=',num2str(T.AcountsComb(T.RefComb==T.Bcomb,:)./T.ReadDepthPass(T.RefComb==T.Bcomb,:),'%-.3f'));
-Info=cellstr(strcat('JPT=',num2str(real(-10*log10(1-mean(P.trust,2))),'%-.1f'),';JPA=',num2str(real(-10*log10(1-mean(P.artifact,2))),'%-.1f'),';JPS=',num2str(P.Somatic(:,1),'%-.5f'),';JPGAB=',num2str(P.Het(:,1),'%-.5f'),';JPGAA=',num2str(P.Hom(:,1),'%-.5f'),';JPND=',num2str(P.NonDip(:,1),'%-.5f')));
+%Info=cellstr(strcat('JPT=',num2str(real(-10*log10(1-mean(P.trust,2))),'%-.1f'),';JPA=',num2str(real(-10*log10(1-mean(P.artifact,2))),'%-.1f'),';JPS=',num2str(P.Somatic(:,1),'%-.5f'),';JPGAB=',num2str(P.Het(:,1),'%-.5f'),';JPGAA=',num2str(P.Hom(:,1),'%-.5f'),';JPND=',num2str(P.NonDip(:,1),'%-.5f')));
+Info=cellstr(strcat('JPT=',strsplit(sprintf('%-.1f\n',real(-10*log10(1-mean(P.trust,2)))))',';JPA=',strsplit(sprintf('%-.1f\n',real(-10*log10(1-mean(P.artifact,2)))))',';JPS=',strsplit(sprintf('%-.5f\n',P.Somatic(:,1)))',';JPGAB=',strsplit(sprintf('%-.5f\n',P.Het(:,1)))',';JPGAA=',strsplit(sprintf('%-.5f\n',P.Hom(:,1)))',';JPND=',strsplit(sprintf('%-.5f\n',P.NonDip(:,1)))'));
 %Info=strcat(Info,';LS=',num2str(P.DataSomatic(:,n),'%-.5f'),';LGAB=',num2str(P.DataHet(:,n),'%-.5f'),';LGAA=',num2str(P.DataHom(:,n),'%-.5f'),';LND=',num2str(P.DataNonDip(:,n),'%-.5f'));
-Info=strcat(Info,';A=',int2ntIndels(T.Acomb),';B=',int2ntIndels(T.Bcomb));
+Info=strcat(Info,';A=',[int2ntIndels(T.Acomb);{''}],';B=',[int2ntIndels(T.Bcomb); {''}]);
 %Info=strcat(Info,';ACountF=',num2str(T.ACountF,'%-.0f'),';ACountR=',num2str(T.ACountR,'%-.0f'),';BCountF=',num2str(T.BCountF,'%-.0f'),';BCountR=',num2str(T.BCountR,'%-.0f'));
-Info=strcat(Info,';ApopAF=',num2str(T.ApopAF,'%-.5f'),';BpopAF=',num2str(T.BpopAF,'%-.5f'),';CosmicCount=',num2str(T.CosmicCount,'%-.0f'));
-Info=strcat(Info,';CloneId=',num2str(cloneId(:,1)));
-Info(delPos)=strcat(Info(delPos),';END=',num2str(endPos(delPos)));
-Info(insPos)=strcat(Info(insPos),';SVLEN=',num2str(svLen(insPos)));
-Info(T.NumCopies~=2 | T.MinAlCopies~=1)=strcat(Info(T.NumCopies~=2 | T.MinAlCopies~=1),';CN=',num2str(T.NumCopies(T.NumCopies~=2 | T.MinAlCopies~=1)),';MACN=',num2str(T.MinAlCopies(T.NumCopies~=2 | T.MinAlCopies~=1)));
+Info=strcat(Info,';ApopAF=',strsplit(sprintf('%-.5f\n',T.ApopAF))',';BpopAF=',strsplit(sprintf('%-.5f\n',T.BpopAF))',';CosmicCount=',strsplit(sprintf('%-.0f\n',T.CosmicCount))');
+Info=strcat(Info,';CloneId=',strsplit(sprintf('%d\n',cloneId(:,1)))');
+Info([delPos; true])=strcat(Info([delPos; true]),';END=',strsplit(sprintf('%d\n',endPos(delPos)))');
+Info([insPos; true])=strcat(Info([insPos; true]),';SVLEN=',strsplit(sprintf('%d\n',svLen(insPos)))');
+cnPos=T.NumCopies~=2 | T.MinAlCopies~=1;
+Info([cnPos; true])=strcat(Info([cnPos; true]),';CN=',strsplit(sprintf('%d\n',T.NumCopies(cnPos)))',';MACN=',strsplit(sprintf('%d\n',T.MinAlCopies(cnPos)))');
+Info=Info(1:height(T));
 %Info=strcat(Info,';PCNA=',num2str(pCNA,'%-.5f'));
 %for i=1:size(F,2)
 %    Info=strcat(Info,';',F.Properties.VariableNames(i),'=',num2str(F{:,i},'%-.3f'));
 %end
+
+gt=repmat('.',height(T),2);
+if inputParam.NormalSample>0
+    bIdx=Tcell{inputParam.NormalSample}.AcountsComb>=Tcell{inputParam.NormalSample}.BcountsComb;
+    aIdx=Tcell{inputParam.NormalSample}.AcountsComb<Tcell{inputParam.NormalSample}.BcountsComb;    
+else
+    bIdx=Tcell{1}.ApopAFcomb>=Tcell{1}.BpopAFcomb;
+    aIdx=Tcell{1}.ApopAFcomb<Tcell{1}.BpopAFcomb;
+end
+gt(bIdx & T.Acomb==T.RefComb,1)='0';
+gt(bIdx & T.Acomb~=T.RefComb,1)='1';
+gt(aIdx & T.Bcomb==T.RefComb,1)='0';
+gt(aIdx & T.Bcomb~=T.RefComb & T.Acomb==T.RefComb,1)='1';
+gt(aIdx & T.Acomb~=T.RefComb & T.Bcomb~=T.RefComb,1)='2';
+gt(bIdx & T.Acomb==T.RefComb,2)='1';
+gt(bIdx & T.Bcomb==T.RefComb,2)='0';
+gt(bIdx & T.Acomb~=T.RefComb & T.Bcomb~=T.RefComb,2)='2';
+gt(aIdx & T.Acomb==T.RefComb,2)='0';
+gt(aIdx & T.Bcomb==T.RefComb,2)='1';
+gt(aIdx & T.Acomb~=T.RefComb & T.Bcomb~=T.RefComb,2)='1';
+
 copyList=unique([T.NumCopies T.MinAlCopies],'rows');
 tumorGT=cell(1,size(T,1));
+T=Tcell{tIdx(1)};
 for i=1:size(copyList,1)
     idx=T.NumCopies==copyList(i,1) & T.MinAlCopies==copyList(i,2);
-    tumorGT(P.Hom(:,1)>0.5 & T.Acomb==T.Ref & idx)={strjoin(cellstr(num2str(zeros(copyList(i,1),1))),'/')};
-    tumorGT(P.Hom(:,1)>0.5 & T.Bcomb==T.Ref & idx)={strjoin(cellstr(num2str(ones(copyList(i,1),1))),'/')};
-    tumorGT(P.Het(:,1)>0.5 & T.Acomb==T.Ref & idx)={strjoin(cellstr(num2str([zeros(copyList(i,1)-copyList(i,2),1); ones(copyList(i,2),1)])),'/')};
-    tumorGT(P.Het(:,1)>0.5 & T.Bcomb==T.Ref & idx)={strjoin(cellstr(num2str([zeros(copyList(i,2),1); ones(copyList(i,1)-copyList(i,2),1)])),'/')};
-    tumorGT(strncmp(Filter,'Somatic',7) & T.cnaF==f(1,cloneId(:,1))' & idx)={strjoin(cellstr(num2str([zeros(copyList(i,2),1); ones(copyList(i,1)-copyList(i,2),1)])),'/')};
+    currSomIdx=strncmp(Filter,'Somatic',7) & T.cnaF==f(tIdx(1),cloneId(:,1))' & idx;
+    if copyList(i,1)>1
+        tumorGT(P.Hom(:,1)>0.5 & idx)=cellstr(repmat(gt(P.Hom(:,1)>0.5 & idx,1),1,copyList(i,1)));
+        tumorGT(currSomIdx)=cellstr(sort([repmat(gt(currSomIdx,1),1,copyList(i,2)) repmat(gt(currSomIdx,2),1,copyList(i,1)-copyList(i,2))],2));
+    elseif copyList(i,1)==1
+        tumorGT(P.Hom(:,1)>0.5 & idx)=cellstr(gt(P.Hom(:,1)>0.5 & idx,1));
+        tumorGT(currSomIdx)=cellstr(gt(currSomIdx,2));
+    else
+        tumorGT(idx)={'.'};
+    end            
+    %tumorGT(P.Hom(:,1)>0.5 & T.Acomb==T.Ref & idx)={strjoin(cellstr(num2str(zeros(copyList(i,1),1))),'/')};
+    %tumorGT(P.Hom(:,1)>0.5 & T.Bcomb==T.Ref & idx)={strjoin(cellstr(num2str(ones(copyList(i,1),1))),'/')};
+    tumorGT(P.Het(:,1)>0.5 & T.Acomb==T.Ref & idx)={strjoin(cellstr(num2str([zeros(copyList(i,1)-copyList(i,2),1); ones(copyList(i,2),1)])),'')};
+    tumorGT(P.Het(:,1)>0.5 & T.Bcomb==T.Ref & idx)={strjoin(cellstr(num2str([zeros(copyList(i,2),1); ones(copyList(i,1)-copyList(i,2),1)])),'')};
+    tumorGT(P.Het(:,1)>0.5 & T.Acomb~=T.Ref & T.Bcomb~=T.Ref & idx)={strjoin(cellstr(num2str([ones(copyList(i,1)-copyList(i,2),1); 2*ones(copyList(i,2),1)])),'')};
+%     tumorGT(strncmp(Filter,'Somatic',7) & T.cnaF==f(1,cloneId(:,1))' & idx)={strjoin(cellstr(num2str([zeros(copyList(i,2),1); ones(copyList(i,1)-copyList(i,2),1)])),'/')};
 end
-tumorGT(strncmp(Filter,'Somatic',7) & T.cnaF~=f(1,cloneId(:,1))')={'0/1'};
+currSomIdx=strncmp(Filter,'Somatic',7) & T.cnaF~=f(tIdx(1),cloneId(:,1))';
+tumorGT(currSomIdx)=cellstr(sort(gt(currSomIdx,:),2));
 tumorGT(cellfun('isempty',tumorGT))={'.'};
+tumorGT=regexprep(tumorGT,'([0-9])','$1\');
+tumorGT=regexprep(tumorGT,'\\$','');
+
 
 germGT=cell(1,size(T,1));
-germGT(P.Hom(:,1)>0.5 & T.Acomb==T.Ref)={'0/0'};
-germGT(P.Hom(:,1)>0.5 & T.Acomb~=T.Ref)={'1/1'};
-germGT(P.Het(:,1)>0.5)={'0/1'};
-germGT(P.Somatic(:,1)>0.5)={'0/0'};
+germGT(P.Hom(:,1)>0.5 | P.Somatic(:,1)>0.5)=cellstr(repmat(gt(P.Hom(:,1)>0.5 | P.Somatic(:,1)>0.5,1),1,2));
+germGT(P.Het(:,1)>0.5)=cellstr(sort(gt(P.Het(:,1)>0.5,:),2));
+% germGT(P.Hom(:,1)>0.5 & T.Acomb==T.Ref)={'0/0'};
+% germGT(P.Hom(:,1)>0.5 & T.Acomb~=T.Ref)={'1/1'};
+% germGT(P.Het(:,1)>0.5)={'0/1'};
+% germGT(P.Somatic(:,1)>0.5)={'0/0'};
 germGT(cellfun('isempty',germGT))={'.'};
+germGT=regexprep(germGT,'([0-9])','$1\');
+germGT=regexprep(germGT,'\\$','');
+
+
 
 
 
@@ -234,7 +280,7 @@ end
 
 %somaticDetected=zeros(size(AF));
 if inputParam.NormalSample<1
-    formatStr(:,1)=strcat(germGT',':.:.:.:.:.:.:.:.:.:.:.');
+    formatStr(:,1)=[strcat(germGT',':.:.:.:.:.:.:.:.:.:.:.'); {''}];
     n=2;
 else
     n=1;
@@ -242,19 +288,19 @@ end
 for i=1:length(Tcell)
     T=Tcell{i};
     if inputParam.NormalSample==i
-        formatStr(:,n)=germGT';
+        formatStr(:,n)=[germGT'; {''}];
     else
 %        n
  %       size(formatStr)
  %       size(tumorGT)
-        formatStr(:,n)=tumorGT';
+        formatStr(:,n)=[tumorGT'; {''}];
     end
-    formatStr(:,n)=strcat(formatStr(:,n),':',num2str(T.ReadDepthPass,'%-.0f'));
+    formatStr(:,n)=strcat(formatStr(:,n),':',strsplit(sprintf('%-.0f\n',T.ReadDepthPass))');
     aIdx=T.RefComb==T.Acomb;
-    formatStr(aIdx,n)=strcat(formatStr(aIdx,n),':',num2str(T.AcountsComb(aIdx),'%-.0f'),',',num2str(T.BcountsComb(aIdx),'%-.0f'));
+    formatStr([aIdx; true],n)=strcat(formatStr([aIdx; true],n),':',strsplit(sprintf('%-.0f\n',T.AcountsComb(aIdx)))',',',strsplit(sprintf('%-.0f\n',T.BcountsComb(aIdx)))');
     bIdx=T.RefComb==T.Bcomb;
-    formatStr(bIdx,n)=strcat(formatStr(bIdx,n),':',num2str(T.BcountsComb(bIdx),'%-.0f'),',',num2str(T.AcountsComb(bIdx),'%-.0f'));
-    formatStr(~aIdx & ~bIdx,n)=strcat(formatStr(~aIdx & ~bIdx,n),':NA,',num2str(T.AcountsComb(~aIdx & ~bIdx),'%-.0f'),',',num2str(T.BcountsComb(~aIdx & ~bIdx),'%-.0f'));
+    formatStr([bIdx; true],n)=strcat(formatStr([bIdx; true],n),':',strsplit(sprintf('%-.0f\n',T.BcountsComb(bIdx)))',',',strsplit(sprintf('%-.0f\n',T.AcountsComb(bIdx)))');
+    formatStr([~aIdx & ~bIdx; true],n)=strcat(formatStr([~aIdx & ~bIdx; true],n),':NA,',strsplit(sprintf('%-.0f\n',T.AcountsComb(~aIdx & ~bIdx)))',',',strsplit(sprintf('%-.0f\n',T.BcountsComb(~aIdx & ~bIdx)))');
     filtStr=repmat({'REJECT'},height(T),1);
     filtStr(P.trust(:,i)>=inputParam.pGoodThresh)={'PASS'};
     filtStr(P.trust(:,i)<inputParam.pGoodThresh & P.artifact(:,i)<inputParam.pGoodThresh)={'LowQC'};
@@ -262,28 +308,29 @@ for i=1:length(Tcell)
         filtStr(somaticDetected(:,i)==1)=strcat(filtStr(somaticDetected(:,i)==1),';SomaticDetected');
         %somaticDetected(P.Somatic(:,i)>0.5 & P.DataSomatic(:,i)>P.DataHom(:,i) & (T.BcountsComb>0 | T.A~=T.RefComb),i)=1;
         filtStr(P.Somatic(:,i)>0.5 & ~somaticDetected(:,i))=strcat(filtStr(P.Somatic(:,i)>0.5 & ~somaticDetected(:,i)),';SomaticNotDetected');
-        formatStr(:,n)=strcat(formatStr(:,n),':',filtStr,':NA');
+        formatStr(:,n)=strcat(formatStr(:,n),':',[filtStr; {''}],':NA');
     else
         %somaticDetected(P.SomaticPair(:,i)>0.5 | (P.Somatic(:,i)>0.5 & P.DataSomatic(:,i)>P.DataHom(:,i) & (T.BcountsComb>0 | T.A~=T.RefComb)),i)=1;
         filtStr(somaticDetected(:,i)==1)=strcat(filtStr(somaticDetected(:,i)==1),';SomaticDetected');
         filtStr(P.Somatic(:,i)>0.5 & ~somaticDetected(:,i))=strcat(filtStr(P.Somatic(:,i)>0.5 & ~somaticDetected(:,i)),';SomaticNotDetected');
-        formatStr(:,n)=strcat(formatStr(:,n),':',filtStr,':',num2str(P.SomaticPair(:,i),'%-.3f'));
+        formatStr(:,n)=strcat(formatStr(:,n),':',[filtStr; {''}],':',strsplit(sprintf('%-.3f\n',P.SomaticPair(:,i)))');
     end
-    formatStr(:,n)=strcat(formatStr(:,n),':',num2str(-10*log10(1-P.trust(:,i)),'%-.0f'),':',num2str(-10*log10(1-P.artifact(:,i)),'%-.0f'),':',num2str(P.DataSomatic(:,i),'%-.0f'));
-    formatStr(aIdx,n)=strcat(formatStr(aIdx,n),':',num2str(-10*log10(P.DataHom(aIdx,i)),'%-.0f'),',',num2str(-10*log10(P.DataHet(aIdx,i)),'%-.0f'),',NA');
-    formatStr(bIdx,n)=strcat(formatStr(bIdx,n),':NA,',num2str(-10*log10(P.DataHet(bIdx,i)),'%-.0f'),',',num2str(-10*log10(P.DataHom(bIdx,i)),'%-.0f'));
+    formatStr(:,n)=strcat(formatStr(:,n),':',strsplit(sprintf('%-.0f\n',-10*log10(1-P.trust(:,i))))',':',strsplit(sprintf('%-.0f\n',-10*log10(1-P.artifact(:,i))))',':',strsplit(sprintf('%-.0f\n',P.DataSomatic(:,i)))');
+    formatStr([aIdx; true],n)=strcat(formatStr([aIdx; true],n),':',strsplit(sprintf('%-.0f\n',-10*log10(P.DataHom(aIdx,i))))',',',strsplit(sprintf('%-.0f\n',-10*log10(P.DataHet(aIdx,i))))',',NA');
+    formatStr([bIdx; true],n)=strcat(formatStr([bIdx; true],n),':NA,',strsplit(sprintf('%-.0f\n',-10*log10(P.DataHet(bIdx,i))))',',',strsplit(sprintf('%-.0f\n',-10*log10(P.DataHom(bIdx,i))))');
     formatStr(~aIdx & ~bIdx,n)=strcat(formatStr(~aIdx & ~bIdx,n),':.');
     if inputParam.NormalSample>0
-        formatStr(:,n)=strcat(formatStr(:,n),':',num2str(-10*log10(P.DataNonDip(:,i)),'%-.0f'));
+        formatStr(:,n)=strcat(formatStr(:,n),':',strsplit(sprintf('%-.0f\n',-10*log10(P.DataNonDip(:,i))))');
     else
         formatStr(:,n)=strcat(formatStr(:,n),':NA');
     end
-    formatStr(strncmp(Filter,'Somatic',7),n)=strcat(formatStr(strncmp(Filter,'Somatic',7),n),':',num2str(sampleFrac(strncmp(Filter,'Somatic',7),i),'%-.3f'));
+    formatStr([strncmp(Filter,'Somatic',7); true],n)=strcat(formatStr([strncmp(Filter,'Somatic',7); true],n),':',strsplit(sprintf('%-.3f\n',sampleFrac(strncmp(Filter,'Somatic',7),i)))');
     formatStr(~strncmp(Filter,'Somatic',7),n)=strcat(formatStr(~strncmp(Filter,'Somatic',7),n),':NA');
     formatStr(T.NumCopies==2 & T.MinAlCopies==1,n)=strcat(formatStr(T.NumCopies==2 & T.MinAlCopies==1,n),':NA');
-    formatStr(T.NumCopies~=2 | T.MinAlCopies~=1,n)=strcat(formatStr(T.NumCopies~=2 | T.MinAlCopies~=1,n),':',num2str(T.cnaF(T.NumCopies~=2 | T.MinAlCopies~=1),'%-.3f'));
+    formatStr([T.NumCopies~=2 | T.MinAlCopies~=1; true],n)=strcat(formatStr([T.NumCopies~=2 | T.MinAlCopies~=1; true],n),':',strsplit(sprintf('%-.3f\n',T.cnaF(T.NumCopies~=2 | T.MinAlCopies~=1)))');
     n=n+1;
 end
+formatStr=formatStr(1:height(T),:);
 
 
 formatFields=repmat({'GT:DP:AD:FT:PPS:PT:PA:PLS:PL:PLND:VSF:CNF'},size(P,1),1);
