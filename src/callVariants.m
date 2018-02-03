120885897539ab708ad8ffcfae49d069585c5921
Rebecca Halperin <rhalperin@tgen.org>
2018-02-03 14:31:10 -0700

    for somatic variants quality scores are waited based on number of reads supporting the variant in a sample

--- src/callVariants.m
+++ src/callVariants.m
@@ -1,4 +1,4 @@
-function [Filter,passPos,somaticDetected]=callVariants(Tcell,P,inputParam)
+function [Filter,passPos,somaticDetected,trustScore,artifactScore]=callVariants(Tcell,P,inputParam)
 
 if inputParam.NormalSample>0
     bIdx=Tcell{inputParam.NormalSample}.AcountsComb>=Tcell{inputParam.NormalSample}.BcountsComb;
@@ -20,14 +20,17 @@ for i=1:length(Tcell)
    somAltCount(somaticDetected(:,i) & bIdx,i)=T.BcountsComb(somaticDetected(:,i) & bIdx);
    somAltCount(somaticDetected(:,i) & aIdx,i)=T.BcountsComb(somaticDetected(:,i) & aIdx);
 end
-[mCount,mIdx]=max(somAltCount,[],2);
+%[mCount,mIdx]=max(somAltCount,[],2);
+trustScore=sum((somAltCount+1).*P.trust,2)./sum(somAltCount+1,2);
+artifactScore=sum((somAltCount+1).*P.artifact,2)./sum(somAltCount+1,2);
+passPos=trustScore>inputParam.pGoodThresh & artifactScore<inputParam.pGoodThresh;
 
 %%% assign filters
-passPos=max(P.trust,[],2)>inputParam.pGoodThresh & max(P.artifact,[],2)<inputParam.pGoodThresh;
-for i=1:length(Tcell)
-    pos=mCount>0 & mIdx==i;
-    passPos(pos)=P.trust(pos,i)>inputParam.pGoodThresh & P.artifact(pos,i)<inputParam.pGoodThresh;
-end
+% passPos=max(P.trust,[],2)>inputParam.pGoodThresh & max(P.artifact,[],2)<inputParam.pGoodThresh;
+% for i=1:length(Tcell)
+%     pos=mCount>0 & mIdx==i;
+%     passPos(pos)=P.trust(pos,i)>inputParam.pGoodThresh & P.artifact(pos,i)<inputParam.pGoodThresh;
+% end
 %trustSom=P.trust;
 %trustSom(~somaticDetected)=0;
 Filter=cell(size(passPos));
@@ -42,7 +45,7 @@ Filter(P.NonDip(:,1)>0.5,:)={'GermlineShiftLowQC'};
 Filter(P.NonDip(:,1)>inputParam.pGermlineThresh & passPos,:)={'GermlineShiftPASS'};
 Filter(P.Somatic(:,1)<0.5 & P.Het(:,1)<0.5 & P.Hom(:,1)<0.5 & P.NonDip(:,1)<0.5,:)={'NoCall'};
 Filter(min(P.artifact,[],2)>inputParam.pGoodThresh,:)={'REJECT'};
-tIdx=setdiff(1:length(Tcell),inputParam.NormalSample)
+tIdx=setdiff(1:length(Tcell),inputParam.NormalSample);
 if inputParam.NormalSample>0
     idxSom=strncmp(Filter,'Somatic',7);
     idxSomPass=strncmp(Filter,'SomaticPASS',11);
