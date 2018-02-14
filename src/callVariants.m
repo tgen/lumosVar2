f3395446a4f90aae166220c20af325bdba07d54c
Rebecca Halperin <rhalperin@tgen.org>
2018-02-14 14:55:21 -0700

    changed quality scores to weighted average by reads supporting alternate allele across samples, and give somaticLowQC priority over SomaticPairPASS

--- src/callVariants.m
+++ src/callVariants.m
@@ -9,7 +9,7 @@ else
 end
 
 somaticDetected=zeros(size(Tcell{1},1),length(Tcell));
-somAltCount=zeros(size(Tcell{1},1),length(Tcell));
+altCount=zeros(size(Tcell{1},1),length(Tcell));
 for i=1:length(Tcell)
     T=Tcell{i};
    if inputParam.NormalSample<1
@@ -17,12 +17,14 @@ for i=1:length(Tcell)
    else
         somaticDetected(P.SomaticPair(:,i)>0.5 | (P.Somatic(:,i)>0.5 & P.DataSomatic(:,i)>P.DataHom(:,i) & (T.BcountsComb>0 | T.A~=T.RefComb)),i)=1;
    end
-   somAltCount(somaticDetected(:,i) & bIdx,i)=T.BcountsComb(somaticDetected(:,i) & bIdx);
-   somAltCount(somaticDetected(:,i) & aIdx,i)=T.BcountsComb(somaticDetected(:,i) & aIdx);
+   altCount(somaticDetected(:,i) & bIdx,i)=T.BcountsComb(somaticDetected(:,i) & bIdx);
+   altCount(somaticDetected(:,i) & aIdx,i)=T.AcountsComb(somaticDetected(:,i) & aIdx);
+   altCount(max([P.Somatic P.SomaticPair],[],2)<0.5 & P.Het(:,i)>0.5,i)=T.BcountsComb(max([P.Somatic P.SomaticPair],[],2)<0.5 & P.Het(:,i)>0.5);
+   altCount(max([P.Somatic P.SomaticPair],[],2)<0.5 & P.Hom(:,i)>0.5,i)=T.AcountsComb(max([P.Somatic P.SomaticPair],[],2)<0.5 & P.Hom(:,i)>0.5);
 end
-%[mCount,mIdx]=max(somAltCount,[],2);
-trustScore=sum((somAltCount+1).*P.trust,2)./sum(somAltCount+1,2);
-artifactScore=sum((somAltCount+1).*P.artifact,2)./sum(somAltCount+1,2);
+%[mCount,mIdx]=max(altCount,[],2);
+trustScore=sum((altCount+1).*P.trust,2)./sum(altCount+1,2);
+artifactScore=sum((altCount+1).*P.artifact,2)./sum(altCount+1,2);
 passPos=trustScore>inputParam.pGoodThresh & artifactScore<inputParam.pGoodThresh;
 
 %%% assign filters
@@ -44,13 +46,15 @@ Filter(P.Hom(:,1)>inputParam.pGermlineThresh & passPos,:)={'GermlineHomPASS'};
 Filter(P.NonDip(:,1)>0.5,:)={'GermlineShiftLowQC'};
 Filter(P.NonDip(:,1)>inputParam.pGermlineThresh & passPos,:)={'GermlineShiftPASS'};
 Filter(P.Somatic(:,1)<0.5 & P.Het(:,1)<0.5 & P.Hom(:,1)<0.5 & P.NonDip(:,1)<0.5,:)={'NoCall'};
-Filter(min(P.artifact,[],2)>inputParam.pGoodThresh,:)={'REJECT'};
+%Filter(min(P.artifact,[],2)>inputParam.pGoodThresh,:)={'REJECT'};
 tIdx=setdiff(1:length(Tcell),inputParam.NormalSample);
 if inputParam.NormalSample>0
     idxSom=strncmp(Filter,'Somatic',7);
-    idxSomPass=strncmp(Filter,'SomaticPASS',11);
+    %idxSomPass=strncmp(Filter,'SomaticPASS',11);
     for i=1:length(tIdx)
-        Filter(P.SomaticPair(:,tIdx(i))>0.5 & ~idxSom & min(P.artifact(:,[tIdx(i) inputParam.NormalSample]),[],2)<inputParam.pGoodThresh,:)={'SomaticPairLowQC'};
-        Filter(max(P.SomaticPair(:,tIdx(i)),[],2)>inputParam.pSomaticThresh & ~idxSomPass & P.trust(:,tIdx(i))>inputParam.pGoodThresh &  max(P.artifact(:,[tIdx(i) inputParam.NormalSample]),[],2)<inputParam.pGoodThresh,:)={'SomaticPairPASS'};
+        %Filter(P.SomaticPair(:,tIdx(i))>0.5 & ~idxSom & min(P.artifact(:,[tIdx(i) inputParam.NormalSample]),[],2)<inputParam.pGoodThresh,:)={'SomaticPairLowQC'};
+        Filter(P.SomaticPair(:,tIdx(i))>0.5 & ~idxSom,:)={'SomaticPairLowQC'};
+        Filter(P.SomaticPair(:,tIdx(i))>inputParam.pSomaticThresh & ~idxSom & passPos)={'SomaticPairPASS'};
     end
 end
+Filter(artifactScore>inputParam.pGoodThresh,:)={'REJECT'};
