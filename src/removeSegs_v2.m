698094f6c303676092033f5012d542031b3e5795
Rebecca Halperin <rhalperin@tgen.org>
2018-05-02 11:25:47 -0700

    segment on BAF in addition to read depth

--- src/removeSegs_v2.m
+++ src/removeSegs_v2.m
@@ -1,4 +1,4 @@
-function segsMerged=removeSegs_v2(segsMerged,exonRD,inputParam,c)
+function segsMerged=removeSegs_v2(segsMerged,exonRD,hetData,inputParam,c)
 
 
 if(sum(segsMerged(:,4)>inputParam.cnaAlpha)==0)
@@ -13,35 +13,49 @@ else
     currRegion(2,:)=segsMerged(idx,1:3);
     if(idx<size(segsMerged,1))
         currRegion(3,:)=segsMerged(idx+1,1:3);
-    end 
+    end
     pos=getPosInRegions([exonRD{1}(:,1) mean(exonRD{1}(:,2:3),2)],currRegion);
+    pos2=getPosInRegions(hetData{1}{:,1:2},currRegion);
     for i=1:length(exonRD)
         mat1(:,i)=exonRD{i}(pos==1,4)./exonRD{i}(pos==1,5);
         mat2(:,i)=exonRD{i}(pos==2,4)./exonRD{i}(pos==2,5);
         mat3(:,i)=exonRD{i}(pos==3,4)./exonRD{i}(pos==3,5);
+        baf1(:,i)=hetData{i}.BcountsComb(pos2==1)./hetData{i}.ReadDepthPass(pos2==1);
+        baf2(:,i)=hetData{i}.BcountsComb(pos2==2)./hetData{i}.ReadDepthPass(pos2==2);
+        baf3(:,i)=hetData{i}.BcountsComb(pos2==3)./hetData{i}.ReadDepthPass(pos2==3);
     end
+    p=ones(1,size(mat1,2));
+    pBaf=ones(1,size(mat1,2));
     if idx>1
         if size(mat1,1)>1
             [~,p]=ttest2(mat1,mat2,inputParam.cnaAlpha);
-            segsMerged(idx-1,4)=min(min(p),1);
         elseif size(mat2,1)>1
             [~,p]=ttest2(mat2,mat1,inputParam.cnaAlpha);
-            segsMerged(idx-1,4)=min(min(p),1);
-        else
-            segsMerged(idx-1,4)=1;
         end
+        if size(baf1,1)>1
+            [~,pBaf]=ttest2(baf1,baf2,inputParam.cnaAlpha);
+        elseif size(baf2,1)>1
+            [~,pBaf]=ttest2(baf2,baf1,inputParam.cnaAlpha);
+        end
+        p=min(p,1);
+        pBaf=min(pBaf,1);
+        segsMerged(idx-1,4)=min(geomean([p pBaf],2),1);
     end
     if idx<size(segsMerged,1)
         if size(mat2,1)>1
             [~,p]=ttest2(mat2,mat3,inputParam.cnaAlpha);
-            segsMerged(idx,4)=min(min(p),1);
         elseif size(mat3,1)>1
             [~,p]=ttest2(mat3,mat2,inputParam.cnaAlpha);
-            segsMerged(idx,4)=min(min(p),1);
-        else
-            segsMerged(idx,4)=1;
         end
+        if size(baf2,1)>1
+            [~,pBaf]=ttest2(baf2,baf3,inputParam.cnaAlpha);
+        elseif size(baf3,1)>1
+            [~,pBaf]=ttest2(baf3,baf2,inputParam.cnaAlpha);
+        end
+        p=min(p,1);
+        pBaf=min(pBaf,1);
+        segsMerged(idx,4)=min(geomean([p pBaf],2),1);
     end
-    c=c+1;
-    segsMerged=removeSegs_v2(segsMerged,exonRD,inputParam,c);
+c=c+1;
+segsMerged=removeSegs_v2(segsMerged,exonRD,hetData,inputParam,c);
 end
--- src/segmentData.m
+++ src/segmentData.m
@@ -1,4 +1,4 @@
-function segs=segmentData(exonRD,cnaAlpha)
+function [segs,bafSegs]=segmentData(exonRD,hetData,cnaAlpha)
 %segmentData - parellizes segmentation by chromosome
 %uses circular binary segmentation from bioinformatics toolbox
 %
@@ -31,11 +31,15 @@ numChr=length(unique(exonRD(:,1)));
 
 parfor i=1:numChr
     clustsegs{i}=cghcbs([exonRD(exonRD(:,1)==i,1) exonRD(exonRD(:,1)==i,2) log((exonRD(exonRD(:,1)==i,4)+1)./(exonRD(exonRD(:,1)==i,5)+1))],'Alpha',cnaAlpha);
+    bafclustsegs{i}=cghcbs([hetData.Chr(hetData.Chr==i) hetData.Pos(hetData.Chr==i)  hetData.BcountsComb(hetData.Chr==i)./hetData.ReadDepthPass(hetData.Chr==i)],'Alpha',cnaAlpha);
 end
     
 segs=[];
+bafSegs=[];
 for i=1:numChr
     segs=[segs; [clustsegs{i}.SegmentData(1).Chromosome*ones(size(clustsegs{i}.SegmentData(1).Start)) clustsegs{i}.SegmentData(1).Start clustsegs{i}.SegmentData(1).End clustsegs{i}.SegmentData(1).Mean]];
+    if (~isempty(bafclustsegs{i}))
+        bafSegs=[bafSegs; [bafclustsegs{i}.SegmentData(1).Chromosome*ones(size(bafclustsegs{i}.SegmentData(1).Start)) bafclustsegs{i}.SegmentData(1).Start bafclustsegs{i}.SegmentData(1).End bafclustsegs{i}.SegmentData(1).Mean]];
+    end
 end
-
 return;
